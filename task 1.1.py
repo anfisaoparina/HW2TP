ea130e6b1173dc745f9f9dd1209c657572c9794d
anfisaoparina <anfaleoparina@edu.hse.ru>
2026-06-02 22:40:28 +0300

    Решено задание 1.2 Написаны все новые функции, добавлен класс Recipe, у которого аттрибутами идут объекты класса Ingredient

--- task 1.1.py
+++ task 1.1.py
@@ -25,12 +25,47 @@ class Ingredient():
             return True
         return False
 
-
 ing = Ingredient('oisk', 100, 'g')
 a = Ingredient('oisk', 30, 'g')
 
 print(ing)
 print(ing == a)
+
+class Recipe():
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                break
+        else:
+            self.ingredients += [ingredient]
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if ratio > 0 and (int(ratio) == ratio or float(ratio) == ratio):
+            return True
+        return False
+
+    def scale(self, ratio):
+        ing = []
+        for i in self.ingredients:
+            ing += [Ingredient(i.name, i.quantity * ratio, i.unit)]
+        return Recipe(self.title, ing)
+
+    def __len__(self):
+        return len(set(self.ingredients))
+
+    def __str__(self):
+        ing = ', '.join(str(x) for x in self.ingredients)
+        return f'{self.title}: {ing}'
+
+rec = Recipe('jnjm', [Ingredient('dss',30,'a'), Ingredient('jks',29, 'a')])
+rec.add_ingredient(Ingredient('dss', 200,'a'))
+print(rec.ingredients)
+print(rec)
 
 
 
