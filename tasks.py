5cc9e7b268aabeb828841583fd7ca34503c7e752
anfisaoparina <anfaleoparina@edu.hse.ru>
2026-06-03 18:48:23 +0300

    Реализация класса DietaryRecipe Написание всех функций этого класса. Базовая проверка работы всех функций созданного класса

--- tasks.py
+++ tasks.py
@@ -25,9 +25,8 @@ class Ingredient():
             return True
         return False
 
-ing = Ingredient('oisk', 100, 'g')
-a = Ingredient('oisk', 30, 'g')
-
+# ing = Ingredient('oisk', 100, 'g')
+# a = Ingredient('oisk', 30, 'g')
 # print(ing)
 # print(ing == a)
 
@@ -62,30 +61,30 @@ class Recipe():
         ing = ', '.join(str(x) for x in self.ingredients)
         return f'{self.title}: {ing}'
 
-rec = Recipe('jnjm', [Ingredient('dss',30,'a'), Ingredient('jks',29, 'a')])
-rec.add_ingredient(Ingredient('dss', 200,'a'))
+# rec = Recipe('jnjm', [Ingredient('dss',30,'a'), Ingredient('jks',29, 'a')])
+# rec.add_ingredient(Ingredient('dss', 200,'a'))
 # print(rec.ingredients)
 # print(rec)
 
 class ShoppingList():
-    def __init__(self, _items_):
-        self.itemss = _items_
+    def __init__(self, _items):
+        self._items = _items
 
-    def addrec(self, recipe, portions):
+    def add_recipe(self, recipe, portions):
         if portions <= 0:
             raise ValueError("Количество порций должно быть положительным")
         recipe = recipe.scale(portions)
         for i in recipe.ingredients:
-            self.itemss += [(i, recipe.title)]
+            self._items += [(i, recipe.title)]
 
     def remove_recipe(self, title):
-        for i in self.itemss.copy():
+        for i in self._items.copy():
             if i[1] == title:
-                self.itemss.remove(i)
+                self._items.remove(i)
 
     def get_list(self):
         d = dict()
-        for i in self.itemss:
+        for i in self._items:
             if (i[0].name, i[0].unit) in d.keys():
                 d[(i[0].name, i[0].unit)] += i[0].quantity
             else:
@@ -93,25 +92,48 @@ class ShoppingList():
         m = []
         for i in d.keys():
             m += [Ingredient(i[0], d[i], i[1])]
-        m =sorted(m, key = lambda x: x.name)
+        m = sorted(m, key = lambda x: x.name)
         return m
 
     def __add__(self, notself):
         new  = ShoppingList([])
-        for i in self.itemss:
-            new.itemss += [i]
-        for j in notself.itemss:
-            new.itemss += [j]
+        for i in self._items:
+            new._items += [i]
+        for j in notself._items:
+            new._items += [j]
         return new
 
-s = ShoppingList([(ing, 'smskm'), (a, 'jssj')])
-g = ShoppingList([(ing, 'jnjwnls'), (a, 'lwlk')])
-s.addrec(rec, 5)
-print(s.itemss)
-s.remove_recipe('jnjm')
-print(s.itemss)
-print(s.get_list())
-print((s + g).itemss)
+# s = ShoppingList([(ing, 'smskm'), (a, 'jssj')])
+# g = ShoppingList([(ing, 'jnjwnls'), (a, 'lwlk')])
+# s.add_recipe(rec, 5)
+# print(s._items)
+# s.remove_recipe('jnjm')
+# print(s._items)
+# print(s.get_list())
+# print((s + g)._items)
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients = None):
+        self.title = title
+        self.diet_type = diet_type
+        super().__init__(title, ingredients)
+
+    def scale(self, ratio):
+        a = super().scale(ratio)
+        dr = DietaryRecipe(self.title, self.diet_type, a.ingredients)
+        return dr
+
+    def __str__(self):
+        return f'[{self.diet_type}]: ' + super().__str__()
+
+# r = DietaryRecipe('aaa', 'jejdk', [ing, a])
+# print(r)
+# print(r.scale(100))
+
+
+
+
+
 
 
 
